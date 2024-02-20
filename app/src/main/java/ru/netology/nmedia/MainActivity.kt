c8cb029595d6219064da9a9d513f8d53b54df855
Vladislav Mishurin <mshrvl17@gmail.com>
2024-02-20 18:54:57 +0300

    add part of lection// try to write additional logic for likes

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,11 +2,27 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
+        findViewById<ImageButton>(R.id.likes).setOnClickListener {
+            (it as ImageButton).setImageResource(R.drawable.ic_liked_24)
+            findViewById<ImageButton>(R.id.likes).setOnClickListener {
+                (it as ImageButton).setImageResource(R.drawable.ic_like_24)
+            }
+                }
+            }
+        }
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        findViewById<ImageButton>(R.id.likes).setOnClickListener {
+            (it as ImageButton).setImageResource(R.drawable.ic_like_24)
+        }
+
     }
 }
-//
