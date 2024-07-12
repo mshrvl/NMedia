d7105bb9a390705518b8ce31720316ebf391071a
mshrvl <mshrvl17@gmail.com>
2024-07-12 11:45:35 +0300

    add dependency

--- app/src/main/java/ru/netology/nmedia/PostsAdapter.kt
+++ app/src/main/java/ru/netology/nmedia/PostsAdapter.kt
@@ -29,6 +29,7 @@ class PostViewHolder(
             likesnumber.text = formatNumber(post.likes)
             repostsnumber.text = formatNumber(post.repostsN)
             likes.setImageResource(if (post.likedByMe) R.drawable.ic_liked_24 else R.drawable.ic_like_24)
+
             likes.setOnClickListener {
                 onInteractionListener.onLike(post)
             }
