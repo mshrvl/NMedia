49c96179b43c8414ed302b579b20b1297b963918
mshrvl <mshrvl17@gmail.com>
2024-07-18 18:56:08 +0300

    add file editpost activity

---
+++ app/src/main/java/activity/EditPostActivity.kt
@@ -0,0 +1,11 @@
+package activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EditPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
